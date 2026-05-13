f68e920da889b0996227d5b75adb3468f4a96939
DewaPuspito <dewactra02@gmail.com>
2026-05-13 14:02:18 +0700

    Ref vs Value Performance Test

---
+++ foreach-ref.php
@@ -0,0 +1,17 @@
+<?php
+
+$largeArray = range(1, 1_000_000);
+$startTime = microtime(true);
+$startMemory = memory_get_usage();
+
+$out = [];
+
+foreach ($largeArray as &$value) {
+    $value = $value * 2;
+}
+
+$endTime = microtime(true);
+$endMemory = memory_get_usage();
+
+echo "Time taken: " . ($endTime - $startTime) . " seconds\n";
+echo "Memory used: " . (($endMemory - $startMemory) / 1024 / 1024) . " MBs\n";
