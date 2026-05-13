8bd171d09c28fe075e8fde4e228ee58ef1bdae2e
DewaPuspito <dewactra02@gmail.com>
2026-05-13 14:08:41 +0700

    ref vs value

--- foreach-ref.php
+++ foreach-ref.php
@@ -1,5 +1,8 @@
 <?php
 
+
+//With References
+
 $largeArray = range(1, 1_000_000);
 $startTime = microtime(true);
 $startMemory = memory_get_usage();
@@ -8,6 +11,24 @@ $out = [];
 
 foreach ($largeArray as &$value) {
     $value = $value * 2;
+}
+
+$endTime = microtime(true);
+$endMemory = memory_get_usage();
+
+echo "Time taken: " . ($endTime - $startTime) . " seconds\n";
+echo "Memory used: " . (($endMemory - $startMemory) / 1024 / 1024) . " MBs\n";
+
+// With Values
+
+$largeArray = range(1, 1_000_000);
+$startTime = microtime(true);
+$startMemory = memory_get_usage();
+
+$out = [];
+
+foreach ($largeArray as $value) {
+    $out[] = $value * 2;
 }
 
 $endTime = microtime(true);
