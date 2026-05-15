26c33e0f448a3583e6a7800e4b8df965c197a407
DewaPuspito <dewactra02@gmail.com>
2026-05-15 13:48:29 +0700

    String Manipulation

---
+++ string-manipulation.php
@@ -0,0 +1,15 @@
+<?php
+
+$str = "Hello, World!";
+// echo $str[0];
+// echo $str[-1];
+
+// echo substr($str, 0, 5);
+// echo substr($str, 5);
+
+// echo strtoupper($str) . "\n";
+// echo strtolower($str) . "\n";
+
+$greeting = "Hello, " .  "World!";
+$greeting .= " Welcome to PHP.";
+echo $greeting . "\n";
