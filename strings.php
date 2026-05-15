df9be7db1967edc66c64d831e9f814ef7d55178a
DewaPuspito <dewactra02@gmail.com>
2026-05-15 11:27:16 +0700

    Strings write method

---
+++ strings.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Alice";
+echo 'Hello, $name!\n';
+echo "Hello, $name!\n";
+
+$heredoc = <<<EOD
+Multi-line string 
+with variable $name
+EOD;
+
+$nowdoc = <<<'EOD'
+Multi-line string 
+with variable $name
+EOD;
+
+echo $heredoc . "\n";
+echo $nowdoc . "\n";
