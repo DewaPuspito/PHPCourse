211137dd4747fe66909612d8c3fd4ab326ec98ee
DewaPuspito <dewactra02@gmail.com>
2026-05-28 14:02:20 +0700

    Abstract Classes

--- interfaces.php
+++ interfaces.php
@@ -8,16 +8,48 @@ interface PaymentProcessor {
 abstract class OnlinePaymentProcessor implements PaymentProcessor {
     public function __construct(protected string $apiKey) {}
 
+    abstract protected function validateApiKey(): bool;
+    abstract protected function executePayment(float $amount): bool;
+    abstract protected function executeRefund(float $amount): bool;
+
     public function processPayment(float $amount): bool {
-        
+        if(!$this->validateApiKey()) {
+            throw new Exception("Invalid API Key");
+        }
+        return $this->executePayment($amount);
     }
 
     public function refundPayment(float $amount): bool {
-        
+        if(!$this->validateApiKey()) {
+            throw new Exception("Invalid API Key");
+        }
+        return $this->executeRefund($amount);
     }
 }
 
-class StripeProcessor extends OnlinePaymentProcessor {}
-class PayPalProcessor extends OnlinePaymentProcessor {}
+class StripeProcessor extends OnlinePaymentProcessor {
+    protected function validateApiKey(): bool {
+        return strpos($this->apiKey, "sk_") === 0;
+    } 
+    
+}
+class PayPalProcessor extends OnlinePaymentProcessor {
+    protected function validateApiKey(): bool {
+        return strlen($this->apiKey) === 32;
+    }
+}
 
-$processor = new StripeProcessor("KEY_");
+class CashPaymentProcessor implements PaymentProcessor {
+    public function processPayment(float $amount): bool {
+        echo "Cash payment processed...";
+        return true;
+    }
+
+    public function refundPayment(float $amount): bool {
+        echo "Cash refund processed...";
+        return true;
+    }
+}
+
+$processor = new StripeProcessor("sk_");
+$processor->processPayment(100);
