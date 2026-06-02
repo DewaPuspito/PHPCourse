68184ef4b997e01e1fcb48a7844c237135a315dc
DewaPuspito <dewactra02@gmail.com>
2026-06-02 14:07:57 +0700

    Final and Readonly

--- interfaces.php
+++ interfaces.php
@@ -1,25 +1,25 @@
 <?php
 
 interface PaymentProcessor {
-    public function processPayment(float $amount): bool;
-    public function refundPayment(float $amount): bool;
+    public function processPayment(float | int $amount): bool;
+    public function refundPayment(float | int $amount): bool;
 } 
 
 abstract class OnlinePaymentProcessor implements PaymentProcessor {
-    public function __construct(protected string $apiKey) {}
+    public function __construct(protected readonly string $apiKey) {}
 
     abstract protected function validateApiKey(): bool;
-    abstract protected function executePayment(float $amount): bool;
-    abstract protected function executeRefund(float $amount): bool;
+    abstract protected function executePayment(float | int $amount): bool;
+    abstract protected function executeRefund(float | int $amount): bool;
 
-    public function processPayment(float $amount): bool {
+    public function processPayment(float | int $amount): bool {
         if(!$this->validateApiKey()) {
             throw new Exception("Invalid API Key" . "\n");
         }
         return $this->executePayment($amount);
     }
 
-    public function refundPayment(float $amount): bool {
+    public function refundPayment(float | int $amount): bool {
         if(!$this->validateApiKey()) {
             throw new Exception("Invalid API Key" . "\n");
         }
@@ -27,40 +27,41 @@ abstract class OnlinePaymentProcessor implements PaymentProcessor {
     }
 }
 
-class StripeProcessor extends OnlinePaymentProcessor {
+final class StripeProcessor extends OnlinePaymentProcessor {
     protected function validateApiKey(): bool {
         return strpos($this->apiKey, "sk_") === 0;
     } 
-    protected function executePayment(float $amount): bool {
+    protected function executePayment(float | int $amount): bool {
         echo "Stripe payment of " . $amount . " processed..." . "\n";
         return true;
     }
-    protected function executeRefund(float $amount): bool {
+    protected function executeRefund(float | int $amount): bool {
         echo "Stripe refund of " . $amount . " processed..." . "\n";
         return true;
     }
 }
+
 class PayPalProcessor extends OnlinePaymentProcessor {
     protected function validateApiKey(): bool {
         return strlen($this->apiKey) === 32;
     }
-    protected function executePayment(float $amount): bool {
+    protected function executePayment(float | int $amount): bool {
         echo "PayPal payment of " . $amount . " processed..." . "\n";
         return true;
     }
-    protected function executeRefund(float $amount): bool {
+    protected function executeRefund(float | int $amount): bool {
         echo "PayPal refund of " . $amount . " processed..." . "\n";
         return true;
     }
 }
 
 class CashPaymentProcessor implements PaymentProcessor {
-    public function processPayment(float $amount): bool {
+    public function processPayment(float | int $amount): bool {
         echo "Cash payment processed..." . "\n";
         return true;
     }
 
-    public function refundPayment(float $amount): bool {
+    public function refundPayment(float | int $amount): bool {
         echo "Cash refund processed..." . "\n";
         return true;
     }
@@ -69,7 +70,15 @@ class CashPaymentProcessor implements PaymentProcessor {
 class OrderProcessor {
     public function __construct(protected PaymentProcessor $paymentProcessor) {}
 
-    public function processOrder(float $amount): void {
+    public function processOrder(float | int $amount, string | array $items): void {
+        if (is_array($items)) {
+           $itemsList = implode(", ", $items);
+        } else {
+            $itemsList = $items;
+        }
+
+        echo "Processing order for items: " . $itemsList . " with amount: " . $amount . "\n";
+
         if ($this->paymentProcessor->processPayment($amount)) {
             echo "Order processed successfully." . "\n";
         } else {
@@ -77,7 +86,7 @@ class OrderProcessor {
         }
     }
 
-    public function refundOrder(float $amount): void {
+    public function refundOrder(float | int $amount): void {
         if ($this->paymentProcessor->refundPayment($amount)) {
             echo "Order refunded successfully." . "\n";
         } else {
@@ -94,9 +103,9 @@ $stripeOrder = new OrderProcessor($stripeProcessor);
 $paypalOrder = new OrderProcessor($paypalProcessor);
 $cashOrder = new OrderProcessor($cashProcessor);
 
-$stripeOrder->processOrder(100.00);
-$paypalOrder->processOrder(150.00);
-$cashOrder->processOrder(50.00);
+$stripeOrder->processOrder(100.00, "Book");
+$paypalOrder->processOrder(150.00, ["Headphones", "Mouse"]);
+$cashOrder->processOrder(50.00, ["Coffee", "Sandwich"]);
 
 $stripeOrder->refundOrder(50.00);
 $paypalOrder->refundOrder(75.00);  
